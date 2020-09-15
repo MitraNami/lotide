7d41b1785a7872dbdd8e92c6def3d7a020e49a39
Mitra Nami <a.mxnami@gmail.com>
2020-09-15 19:41:32 +0000

    added eqArrays function which compares two simple arrays

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  let output;
+  const greenTick = String.fromCodePoint(0x2705);
+  const redCross = String.fromCodePoint(0x274C);
+  if (actual === expected) {
+    output = `${greenTick}Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    output = `${redCross} Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(output);
+};
+
+const eqArrays = function(array1, array2) {
+  let equality = true;
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        equality = false;
+        break;
+      }
+    }
+  } else {
+    equality = false;
+  }
+  return equality;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
