0174e1ec79d720f704bf48b43b6de92ef2b5ec34
Mitra Nami <a.mxnami@gmail.com>
2020-09-20 00:32:50 +0000

    refactored eqArrays.js and adedd the test code to eqArraysTest.js in test directory

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let output;
-  const greenTick = String.fromCodePoint(0x2705);
-  const redCross = String.fromCodePoint(0x274C);
-  if (actual === expected) {
-    output = `${greenTick}Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    output = `${redCross} Assertion Failed: ${actual} !== ${expected}`;
-  }
-  console.log(output);
-};
-
 const eqArrays = function(array1, array2) {
   let equality = true;
   if (array1.length === array2.length) {
@@ -25,5 +13,4 @@ const eqArrays = function(array1, array2) {
   return equality;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays
