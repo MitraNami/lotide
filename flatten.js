7d4df27dd4a7620c3c13ac67e43a3a5fd34d51ec
Mitra Nami <49569563+MitraNami@users.noreply.github.com>
2020-12-16 19:02:15 -0500

    Update flatten.js
    Add a solution with recursion for deep nesting. Also, add a solution using higher order function for one-level nesting.

--- flatten.js
+++ flatten.js
@@ -1,16 +1,47 @@
 //flatten gets a nested array and returns an un-nested array
-//flatten function only handles one level of nesting
 
-const flatten = function(array) {
-  let output = [];
-  for (let element of array) {
-    if (Array.isArray(element)) {
-      output = output.concat(element);
-    } else {
-      output.push(element);
+// recursion: handles deep nesting of arrays
+// const flatten = function(arr) {
+//   let flattenedArray = [];
+//   for (const item of arr) {
+//     if (Array.isArray(item)) {
+//       flattenedArray = flattenedArray.concat(flatten(item));
+//     }
+//     else {
+//       flattenedArray.push(item);
+//     }
+//   }
+//   return flattenedArray;
+// };
+
+// const flattened = flatten([[1, 2], [3, [[4], 5]], [[[[6]]]]]);
+// assertArraysEqual(flattened, [1, 2, 3, 4, 5, 6]);
+
+
+// only handles one level of nesting:
+const flatten = function(arr) {
+  let flattenedArray = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      flattenedArray = flattenedArray.concat(item);
+    }
+    else {
+      flattenedArray.push(item);
     }
   }
-  return output;
+  return flattenedArray;
 };
+
+// only handles one level of nesting (higher-order function):
+// const flatten = function(arr) {
+//   return arr.reduce((acc, item) => {
+//     if (Array.isArray(item)) {
+//       return acc.concat(item);
+//     }
+//     acc.push(item);
+//     return acc;
+//   }, []);
+// };
+
 
 module.exports = flatten;
