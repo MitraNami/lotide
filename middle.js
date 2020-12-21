c3a0ff463f958ffcc5055f0bfb78f560bf08b32f
Mitra Nami <49569563+MitraNami@users.noreply.github.com>
2020-12-21 01:38:37 -0500

    Update middle.js

--- middle.js
+++ middle.js
@@ -3,15 +3,18 @@
 //midle elements if the array has even number of elements
 
 const middle = function(array) {
-  const output = [];
   const length = array.length;
-  if (length % 2 === 0 && length !== 2) {
-    output.push(array[length / 2 - 1]);
+  const midd = [];
+  if (length === 1 || length === 2) {
+    return midd;
   }
-  if (length !== 1 && length !== 2) {
-    output.push(array[Math.floor(length / 2)]);
+  const middleIndex = Math.floor(length / 2);
+  midd.push(array[middleIndex]);
+  if (length % 2 === 0) {
+    const index = middleIndex - 1;
+    midd.unshift(array[index]);
   }
-  return output;
+  return midd;
 };
 
 
