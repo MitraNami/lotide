197428d8d7b3f4074505a701ff71b52eeb3628a5
Mitra Nami <49569563+MitraNami@users.noreply.github.com>
2020-12-24 15:40:24 -0500

    Return instead of using break

--- eqObjects.js
+++ eqObjects.js
@@ -1,32 +1,30 @@
-
 const eqArrays = require('./eqArrays');
 
-
 // Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-  let equality = true;
-  if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (let key of Object.keys(object1)) {
+// Otherwise you get false
+const eqObjects = function(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (const key of keys1) {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
 
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        //both values are arrays
-        if (!eqArrays(object1[key], object2[key])) {
-          equality = false;
-          break;
-        }
-      } else {
-        //assuming values are primitives if they're not arrays
-        if (object1[key] !== object2[key]) {
-          equality = false;
-          break;
-        }
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      //both values are arrays
+      if (!eqArrays(value1, value2)) {
+        return false
+      }
+    } else {
+      //assuming values are primitives if they're not arrays
+      if (value1 !== value2) {
+        return false;
       }
     }
-  } else {
-    equality = false;
   }
-  return equality;
+  return true;
 };
 
 
